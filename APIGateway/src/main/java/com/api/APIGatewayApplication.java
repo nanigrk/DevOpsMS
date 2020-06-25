373fa7903784566b6d1f713ea3575c1cf8660816
nanigrk <nanigrk@gmail.com>
2020-06-25 12:56:10 +0530

    update

--- APIGateway/src/main/java/com/api/APIGatewayApplication.java
+++ APIGateway/src/main/java/com/api/APIGatewayApplication.java
@@ -3,11 +3,13 @@ package com.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableDiscoveryClient
+@EnableHystrix
 public class APIGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(APIGatewayApplication.class, args);
