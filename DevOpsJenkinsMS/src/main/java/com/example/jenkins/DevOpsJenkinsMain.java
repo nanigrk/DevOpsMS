2c65f8f67bb37cd56892476136a088745794bf54
nanigrk <nanigrk@gmail.com>
2020-06-25 12:58:34 +0530

    Update DevOpsJenkinsMain.java

--- DevOpsJenkinsMS/src/main/java/com/example/jenkins/DevOpsJenkinsMain.java
+++ DevOpsJenkinsMS/src/main/java/com/example/jenkins/DevOpsJenkinsMain.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableCircuitBreaker
-@EnableHystrixDashboard
+@EnableHystrix
 public class DevOpsJenkinsMain {
 
 	public static void main(String[] args) {
